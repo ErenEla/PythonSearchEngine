4de2b863e78ff1a2069319ab20302b793e0a95fa
ErenEla <ela.eren1@gmail.com>
2019-03-10 18:02:01 +0300

    week_hours

---
+++ Unit1_Hw1.py
@@ -0,0 +1,9 @@
+# Write Python code that prints out the number of hours in 7 weeks.
+#(I wanted to take it one step further)
+
+hour = 24 
+day_hours = 1*hour
+week_hours = 7*day_hours
+print("How many weeks to calculate?")
+week_amount = int(input())
+print(week_hours*week_amount)
